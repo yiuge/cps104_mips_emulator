6cbae26c067d132881357e19b379e5332d3d3dcb
Evan <evan@user-152-3-158-103.wireless.duke.edu>
2009-04-22 13:08:40 -0400

    working on syscalls

--- emulator.cc
+++ emulator.cc
@@ -268,12 +268,15 @@ void syscall() {
 	int v0 = registers[2]; // register 2 is v0
 	switch (v0) {
 	case 1:
-	  printf("%d", registers[4]); //registers 4-7 are a0-a3
+	  int tobePrinted = getAddress(registers[4]);
+
+	  printf("%d", tobePrinted); //registers 4-7 are a0-a3
 	  //cout << dec << endl;
 	  //cout << registers[4] << endl;
 	break;
 	case 4:
-	  	printf("%s", registers[4]); //registers 4-7 are a0-a3
+	  int toPrint = getAddress(registers[4]);
+	  	printf("%s", toPrint); //registers 4-7 are a0-a3
 		//cout << registers[4] << endl;
 	  break;
 	case 5:
@@ -536,6 +539,10 @@ void readFile(string filename) {
 		sscanf(firstStr.c_str(), "%x", &firstInt);
 		sscanf(secondStr.c_str(), "%x", &secondInt);
 		storeAddress(firstInt, secondInt);
+// 	storeAddress(firstInt + 3, (secondInt & 0xFF));
+// 	storeAddress(firstInt + 2, (((secondInt & 0xFF00) >> 8)));
+// 	storeAddress(firstInt + 1, (((secondInt & 0xFF0000) >> 16)));
+// 	storeAddress(firstInt, (((secondInt & 0xFF000000) >> 24)));
 	}
 
 }
